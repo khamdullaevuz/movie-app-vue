8692e76ee5b1c84eeafc54a1edb6c44b988f55a6
khamdullaevuz <elbek.khamdullaev@gmail.com>
2023-04-20 17:24:11 +0500

    customizing views

--- src/components/movie-list/MovieList.vue
+++ src/components/movie-list/MovieList.vue
@@ -1,8 +1,6 @@
 <template>
   <ul class="movie-list list-group">
-    <MovieListItem />
-    <MovieListItem />
-    <MovieListItem />
+    <MovieListItem v-for="movie in movies" :movie="movie" />
   </ul>
 </template>
 
@@ -11,6 +9,30 @@ import MovieListItem from "../movie-list-item/MovieListItem.Vue";
 export default {
   components: {
     MovieListItem,
+  },
+  data() {
+    return {
+      movies: [
+        {
+          name: "Omar",
+          viewers: 811,
+          favourite: false,
+          like: true,
+        },
+        {
+          name: "Empire of osman",
+          viewers: 411,
+          favourite: false,
+          like: false,
+        },
+        {
+          name: "Ertugrul",
+          viewers: 711,
+          favourite: true,
+          like: false,
+        },
+      ],
+    };
   },
 };
 </script>
