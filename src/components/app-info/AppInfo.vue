ba27e2c56df224b973562e602e3207cb565b76ba
khamdullaevuz <elbek.khamdullaev@gmail.com>
2023-04-20 15:30:50 +0500

    adding project to git

---
+++ src/components/app-info/AppInfo.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>App info</h1>
+</template>
+
+<script></script>
+
+<style></style>
---
+++ src/components/search-panel/SearchPanel.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Search Panel</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
